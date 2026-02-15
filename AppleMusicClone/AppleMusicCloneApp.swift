e7fd963b5c7d8273884056c37624f2ad4d2e147b
Artem Golovchenko <voiced.fair-0a@icloud.com>
2026-02-15 18:27:52 +0200

    Initial Commit

---
+++ AppleMusicClone/AppleMusicCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicCloneApp.swift
+//  AppleMusicClone
+//
+//  Created by Artem Golovchenko on 15.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusicClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleMusicClone
+//
+//  Created by Artem Golovchenko on 15.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
